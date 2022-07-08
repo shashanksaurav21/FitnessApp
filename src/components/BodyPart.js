5fe18357930a5e4de3ed2a84fe453cfc5e6d5814
shashanksaurav21 <83392991+shashanksaurav21@users.noreply.github.com>
2022-07-09 02:38:12 +0530

    Horizonatal scroll complete
    Horizontal Scroll complete with Body part implementation

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Stack, Typography } from '@mui/material'
+import Icon from '../assets/icons/gym.png'
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+      type='button'
+      alignItems='center'
+      justifyContent='center'
+      className='bodyPart-card'
+      sx={{
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+        backgroundColor: '#fff',
+        borderBottonLeftRadius: '20px',
+        width: '270px',
+        height: '280px',
+        cursor: 'pointer',
+        gap: '47px'
+        }}
+        onClick= {() =>{
+          setBodyPart(item)
+          window.scrollTo9({ top:1800, left:100, behaviour:'smooth' })
+        }}
+        >
+      <img src={Icon} alt='dumbell' style={{ width:'40px', height: '40px' }} />
+      <Typography fontSize='24px' fontWeight='bold' color='#3A1212'
+        textTransform='capitalize' >
+        {item}
+      </Typography>
+    </Stack>
   )
 }
 
