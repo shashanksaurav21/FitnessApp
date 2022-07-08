3ba7cc752c5f6b4d20144c7f1d9bd4973754fc4d
shashanksaurav21 <83392991+shashanksaurav21@users.noreply.github.com>
2022-07-09 01:48:03 +0530

    HorizontalScrollbar
    making horizontal scrollbar

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import { Box } from '@mui/system'
+
+import Exercises from '../components/Exercises'
+import SearchExercises from '../components/SearchExercises'
+import HeroBanner from '../components/HeroBanner'
+
+const Home = () => {
+
+  const [ exercises, setExercises ] = useState([])
+  const [ bodyPart, setBodyPart ] = useState('all')
+  return (
+    <Box>
+        <HeroBanner />
+        <SearchExercises 
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}/>
+        <Exercises 
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}/>
+    </Box>
+  )
+}
+
+export default Home
